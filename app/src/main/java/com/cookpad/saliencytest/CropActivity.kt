231ddd211238d79b1ff63b9ad9523ca004f2b517
Victor Albertos <victor-albertos@cookpad.com>
2019-02-10 10:19:40 +0100

    checkpoint

--- app/src/main/java/com/cookpad/saliencytest/CropActivity.kt
+++ app/src/main/java/com/cookpad/saliencytest/CropActivity.kt
@@ -1,6 +1,5 @@
 package com.cookpad.saliencytest
 
-import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -16,22 +15,29 @@ class CropActivity : AppCompatActivity() {
         setContentView(R.layout.activity_crop)
 
         var original = (ivOriginal.drawable as BitmapDrawable).bitmap
-        original = Bitmap.createScaledBitmap(original, 1000,500, true)
+        //original = Bitmap.createScaledBitmap(original, 500,1000, true)
 
         LayoutInflater.from(this).inflate(R.layout.item_crop, llContainer)
         val item =  llContainer.getChildAt(llContainer.childCount-1)
-        item.findViewById<ImageView>(R.id.ivCrop).setImageBitmap(Utils.crop(original, 0f, 1f))
+
+        val width = pxFromDp(200)
+        val height = pxFromDp(100)
+        //item.findViewById<ImageView>(R.id.ivCrop).setImageBitmap(Utils.crop(original,.0f, .0f, size, size))
 
 
-        for (i in 0..10) {
+        for (i in 0..9) {
             val x = i * .1f
             val y = i * .1f
 
             LayoutInflater.from(this).inflate(R.layout.item_crop, llContainer)
             val item =  llContainer.getChildAt(llContainer.childCount-1)
             item.findViewById<TextView>(R.id.tvCrop).text = "x:$x,y:$y"
-            item.findViewById<ImageView>(R.id.ivCrop).setImageBitmap(crop(original, x, y))
+            item.findViewById<ImageView>(R.id.ivCrop).setImageBitmap(crop(original, x, y, width, height))
         }
+    }
+
+    fun pxFromDp(dp: Int): Int {
+        return (dp * getResources().getDisplayMetrics().density).toInt()
     }
 
 
--- app/src/main/java/com/cookpad/saliencytest/Utils.kt
+++ app/src/main/java/com/cookpad/saliencytest/Utils.kt
@@ -5,7 +5,13 @@ import android.graphics.Bitmap
 import java.io.FileInputStream
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
+import kotlin.math.max
+import kotlin.math.min
 
+
+enum class Orientation {
+    Landscape, Portrait, Square
+}
 
 object Utils {
     fun loadModelFile(activity: Activity, MODEL_FILE: String): MappedByteBuffer {
@@ -17,39 +23,69 @@ object Utils {
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
     }
 
-    fun crop(original: Bitmap, x: Float, y: Float): Bitmap {
-        val windowPct = .2f
-
-        val cropped = original.run {
-            val croppedWith = (width * windowPct).toInt()
-            val croppedHeight = (height * windowPct).toInt()
-
-            val xWith = (x * width - croppedWith * .5).toInt()
-            val yHeight = (y * height - croppedHeight * .5).toInt()
-
-            val xAdjusted = if (xWith + croppedWith > width) {
-                width - croppedWith
+    fun crop(original: Bitmap, x: Float, y: Float, targetWith: Int, targetHeight: Int): Bitmap {
+        val resizedBitmap = when {
+            targetWith >= targetHeight -> if (original.width < targetWith) {
+                val newWidth = targetWith * 1
+                val ratio = original.width.toFloat() / newWidth.toFloat()
+                Bitmap.createScaledBitmap(original, newWidth, (original.height / ratio).toInt(), true)
             } else {
-                maxOf(xWith, 0)
+                val newWidth = (targetWith * 1.5).toInt()
+                val ratio = original.width.toFloat() / newWidth.toFloat()
+                Bitmap.createScaledBitmap(original, newWidth, (original.height / ratio).toInt(), true)
             }
-
-            val yAdjusted = if (yHeight + croppedHeight > height) {
-                height - croppedHeight
+            else -> if (original.height < targetHeight) {
+                val newHeight = targetHeight * 1
+                val ratio = original.height.toFloat() / newHeight.toFloat()
+                Bitmap.createScaledBitmap(original, (original.width / ratio).toInt(), newHeight, true)
             } else {
-                maxOf(yHeight, 0)
+                original
             }
-
-            Bitmap.createBitmap(
-                this,
-                xAdjusted,
-                yAdjusted,
-                croppedWith,
-                croppedHeight
-            )
         }
 
+        val xWith = max((x * resizedBitmap.width - targetWith * .5).toInt(), 0)
+        val croppedWith = targetWith + min(xWith, 0)
+        val xAdjusted = max(xWith, 0) - max(xWith + croppedWith - resizedBitmap.width, 0)
 
-        return cropped
+        val yHeight = max((y * resizedBitmap.height - targetHeight * .5).toInt(), 0)
+        val croppedHeight = targetHeight + min(yHeight, 0)
+        val yAdjusted = max(yHeight, 0) - max(yHeight + croppedHeight - resizedBitmap.height, 0)
+
+        return Bitmap.createBitmap(
+            resizedBitmap,
+            xAdjusted,
+            yAdjusted,
+            croppedWith,
+            croppedHeight
+        )
+    }
+
+    fun crop(original: Bitmap, x: Float, y: Float, windowPct: Float = .1f) = original.run {
+        val croppedWith = (width * windowPct).toInt()
+        val croppedHeight = (height * windowPct).toInt()
+
+        val xWith = (x * width - croppedWith * .5).toInt()
+        val yHeight = (y * height - croppedHeight * .5).toInt()
+
+        val xAdjusted = if (xWith + croppedWith > width) {
+            width - croppedWith
+        } else {
+            maxOf(xWith, 0)
+        }
+
+        val yAdjusted = if (yHeight + croppedHeight > height) {
+            height - croppedHeight
+        } else {
+            maxOf(yHeight, 0)
+        }
+
+        Bitmap.createBitmap(
+            this,
+            xAdjusted,
+            yAdjusted,
+            croppedWith,
+            croppedHeight
+        )
     }
 }
 
