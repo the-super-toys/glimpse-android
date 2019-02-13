18dd1ed0a9194dd42aadcdfc4b4d1106dff06ac7
Victor Albertos <victor-albertos@cookpad.com>
2019-02-13 01:50:37 +0100

    adjust glide and picasso transformation

--- glimpse-glide/src/main/java/glimpse/glide/GlimpseTransformation.kt
+++ glimpse-glide/src/main/java/glimpse/glide/GlimpseTransformation.kt
@@ -6,12 +6,12 @@ import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import com.bumptech.glide.util.Util
 import glimpse.core.crop
+import glimpse.core.findCenter
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
 import java.security.MessageDigest
-import kotlin.random.Random
 
-class GlimpseTransformation(private val zoom: Float = 1f) : BitmapTransformation() {
+class GlimpseTransformation(private val optimizeZoom: Boolean) : BitmapTransformation() {
 
     companion object {
         private val id = "glimpse.glide.transformation"
@@ -19,22 +19,27 @@ class GlimpseTransformation(private val zoom: Float = 1f) : BitmapTransformation
     }
 
     override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap {
-        val x = Random.nextDouble(0.0, 1.0).toFloat()
-        val y = Random.nextDouble(0.0, 1.0).toFloat()
-        return toTransform.crop(x, y, outWidth, outHeight)
+        if (toTransform.width == outWidth || toTransform.height == outHeight
+            || outWidth == 0 || outWidth == 1
+            || outHeight == 0 || outHeight == 1
+        ) {
+            return toTransform
+        }
+
+        val (x, y) = toTransform.findCenter()
+        return toTransform.crop(x, y, outWidth, outHeight, optimizeZoom = optimizeZoom)
     }
 
     override fun equals(other: Any?): Boolean = if (other is GlimpseTransformation) {
-        zoom == other.zoom
+        optimizeZoom == other.optimizeZoom
     } else {
         false
     }
 
-    override fun hashCode(): Int = Util.hashCode(ID.hashCode(), Util.hashCode((zoom * 100).toInt()))
+    override fun hashCode(): Int = Util.hashCode(ID.hashCode(), Util.hashCode(optimizeZoom))
 
     override fun updateDiskCacheKey(messageDigest: MessageDigest) {
         messageDigest.update(idBytes)
-
-        messageDigest.update(ByteBuffer.allocate(4).putFloat(zoom).array())
+        messageDigest.update(ByteBuffer.allocate(4).putInt(if (optimizeZoom) 1 else 0).array())
     }
 }
--- glimpse-picasso/src/main/java/glimpse/picasso/GlimpseTransformation.kt
+++ glimpse-picasso/src/main/java/glimpse/picasso/GlimpseTransformation.kt
@@ -4,8 +4,9 @@ import android.graphics.Bitmap
 import android.widget.ImageView
 import com.squareup.picasso.Transformation
 import glimpse.core.crop
+import glimpse.core.findCenter
 
-class GlimpseTransformation(target: ImageView, private val zoom: Float = 1f) : Transformation {
+class GlimpseTransformation(target: ImageView, private val optimizeZoom: Boolean) : Transformation {
     companion object {
         private val id = "glimpse.picasso.transformation"
     }
@@ -14,12 +15,20 @@ class GlimpseTransformation(target: ImageView, private val zoom: Float = 1f) : T
     private val targetHeight by lazy { target.layoutParams.height }
 
     override fun transform(source: Bitmap): Bitmap {
-        return source.crop(0.1f, 0.1f, targetWidth, targetHeight).also {
+        if (source.width == targetWidth || source.height == targetHeight
+            || targetWidth == 0 || targetWidth == 1
+            || targetHeight == 0 || targetHeight == 1
+        ) {
+            return source
+        }
+
+        val (x, y) = source.findCenter()
+        return source.crop(x, y, targetWidth, targetHeight, optimizeZoom = optimizeZoom).also {
             source.recycle()
         }
     }
 
     override fun key(): String {
-        return "$id$targetWidth,$targetHeight,$zoom"
+        return "$id$targetWidth,$targetHeight,$optimizeZoom"
     }
 }
